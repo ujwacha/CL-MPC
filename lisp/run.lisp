e53607668a14e3ddca5af7b3e5ec1656f4dc9729
Ujwol Acharya <ujwolacharya4@gmail.com>
2026-03-05 00:34:10 +0545

    Finally runs

--- lisp/run.lisp
+++ lisp/run.lisp
@@ -1,5 +1,11 @@
+(load "symbolic.lisp")
+(load "matrix-builder.lisp")
+(load "osqp.lisp")
+(load "udp.lisp")
+
+
 (defparameter *g*     9.8d0)   ; gravity positive
-(defparameter *r*     0.1d0)    ; pendulum length
+(defparameter *r*     1.0d0)    ; pendulum length
 (defparameter *invr*   (/ 1.0d0 *r*))   ; 1/r
 (defparameter *delt*   0.02d0)
 
@@ -11,8 +17,10 @@
 (defparameter *state-weights*
   '((10   0   0   0)
     (0    0.5   0   0)
-    (0    0   3   0)
+    (0    0   10   0)
     (0    0   0   0.1)))
+
+
 
 ;; Control weights  R  (1x1)
 (defparameter *control-weights*
@@ -20,15 +28,19 @@
 
 ;; Control limits  [a_min, a_max]
 (defparameter *control-limits*
-  '((-30.0) (30.0)))
+  '((-50.0) (50.0)))
 
 (defparameter *state-limits*
   (state-limit-symbolic->list
    *states*
-   -0.5
-   '((+ theta))
-   +0.5)
+   -70000.0
+   '((+ 0))
+   +70000.0)
   )
+
+
+
+
 
 ;; State transition (symbolic, delt/g/invr2 spliced in at load time)
 (defparameter *state-transition*
@@ -59,34 +71,27 @@
                 (jacobian *controls* *state-transition*))))
 
 
-(defparameter *current-state* '(-0.3 0 5 0))
-(defparameter *current-control* '(0))
+(defparameter *initial-state* 'nil)
+(setf *initial-state* '(0.0 0 0 0))
+(defparameter *initial-control* 'nil)
+(setf *initial-control* '(0))
 
 
-(jacobian *states* *state-transition*)
-(jacobian *controls* *state-transition*)
-
-
-(apply *a-fn* (append *current-state* *current-control*))
-
-;; now make initial matrices for the MPC
-;; make Q vector
+(defvar *initial-q* nil)
+(defvar *initial-p* nil)
 
 
 (defun trajectory-to-state-q (current-state final-state)
   (let ((traj (loop for i from 0 to *horizon*
 			collect final-state)))
-	(get-q *horizon* traj current-state '(theta omega x v) '(a))))
+	(get-q *horizon*
+	       traj
+	       current-state
+	       '(theta omega x v)
+	       '(a)
+	       (get-diagonal-elements *state-weights*))))
 
 
-(defvar *initial-q* nil)
-
-(setf *initial-q* 
-      (trajectory-to-state-q *current-state* '(0 0 2.5 0)))
-
-
-
-(defvar *initial-p* nil)
 
 (defun crc-to-function-helper (crc-list)
   (let ((output crc-list))
@@ -116,82 +121,18 @@
 	 (controls-values (group control-seq control-len)))
     (list states-values controls-values)))
 
-(setf *initial-p*
-      (crc-to-function-helper (list-matrix->upper-tirangular-csc-list
-			       (get-p *horizon* *state-weights* *control-weights*))))
-
-
-
-
-
-(apply *b-fn* (append *current-state* *current-control*))
-
-(let ((current-state-jacobian
-	(apply *a-fn* (append *current-state* *current-control*)))
-      (current-control-jacobian
-	(apply *b-fn* (append *current-state* *current-control*))))
-  (list-matrix->csc-list
-   (car 
-    (get-a-l-u-matrix *horizon*
-		      current-state-jacobian
-		      current-control-jacobian
-		      *states*
-		      *current-state*
-		      *controls*))))
 
 
 (defvar *current-alu* 'nil)
-
-(setf *current-alu*
-  (let* ((current-state-jacobian
-	   (apply *a-fn* (append *current-state* *current-control*)))
-	 (current-control-jacobian
-	   (apply *b-fn* (append *current-state* *current-control*)))
-	 (alu-matrix 
-	   (get-a-l-u-matrix *horizon*
-			     current-state-jacobian
-			     current-control-jacobian
-			     *states*
-			     *current-state*
-			     *controls*
-			     *control-limits*
-			     *state-limits*
-			     )))
-    alu-matrix))
-
-
-
 (defvar *current-a* nil)
 (defvar *current-l* nil)
 (defvar *current-u* nil)
 
 
 
-(destructuring-bind (a l u) *current-alu*
-  (setf *current-a* a)
-  (setf *current-l* l)
-  (setf *current-u* u))
 
-
-(let* ((n (length *current-a*))
-       (m (length (car *current-a*)))
-       (dim (list m n))
-       (p *initial-p*)
-       (q *initial-q*)
-       (a  (crc-to-function-helper
-	    (list-matrix->csc-list *current-a*)))
-       (l *current-l*)
-       (u *current-u*))
-  (print "setting up mpc problem")
-  (print m)
-  (print n)
-  (set-mpc-problem dim
-		   p
-		   q
-		   a
-		   l
-		   u))
-
+(defvar *target-state* nil)
+(setf *target-state* '(0 0 5 0))
 
 
 
@@ -231,49 +172,284 @@
 	       ))))
 
 
+
+
+(defun initialize-mpc-system ()
+  
+  (setf *initial-q* 
+	(trajectory-to-state-q *initial-state* *target-state*))
+
+  (setf *initial-p*
+	(crc-to-function-helper (list-matrix->upper-tirangular-csc-list
+				 (get-p *horizon* *state-weights* *control-weights*))))
+
+  
+  (setf *current-alu*
+	(let ((current-state-jacobian
+		(apply *a-fn* (append *initial-state* *initial-control*)))
+	      (current-control-jacobian
+		(apply *b-fn* (append *initial-state* *initial-control*))))
+	  (get-a-l-u-matrix *horizon*
+			    current-state-jacobian
+			    current-control-jacobian
+			    *states*
+			    *initial-state*
+			    *controls*
+			    *control-limits*
+			    *state-limits*)))
+
+  (destructuring-bind (a l u) *current-alu*
+    (setf *current-a* a)
+    (setf *current-l* l)
+    (setf *current-u* u))
+
+
+
+  (let* ((n (length *current-a*))
+	 (m (length (car *current-a*)))
+	 (dim (list m n))
+	 (p *initial-p*)
+	 (q *initial-q*)
+	 (a  (crc-to-function-helper
+	      (list-matrix->csc-list *current-a*)))
+	 (l *current-l*)
+	 (u *current-u*))
+    (print "setting up mpc problem")
+    (print m)
+    (print n)
+    (set-mpc-problem dim p q a l u))
+
+  (run-mpc-once)
+  (try-converge-non-linear *horizon*))
+
+
+;; (mpc-matrix-update-p (nth 3 (list-matrix->csc-list (get-p *horizon* *state-weights* *control-weights*))))
+
+
+(setf *state-weights*
+      '((30   0   0   0)
+       (0    0.5   0   0)
+       (0    0   5   0)
+       (0    0   0   0.1)))
+
+
+(initialize-mpc-system)
+
+
+(defun mpc-next-goto (objective)
+  (if (not (= (length *states*)
+	      (length objective)))
+      (error "Dimention of or objective wrong"))
+  
+  (destructuring-bind (states controls) (solved-to-proper-list
+					 (solve-mpc)
+					 *states*
+					 *controls*
+					 *horizon*)
+    
+    (let* ((next-state ;; (car (cdr states))
+	     (apply *f-fn* (append (car states) (car controls))))
+	   (next-control (car (cdr controls)))
+	   (planned-traj (trajectory-to-state-q next-state
+						objective))
+	   (state-control-list (mapcar (lambda (x y) (append x y))
+				       (cdr states) controls))
+	   
+	   (new-jac-states (mapcar (lambda (x) (apply *a-fn* x))
+				   state-control-list))
+	   
+	   (new-jac-controls (mapcar (lambda (x) (apply *b-fn* x))
+				     state-control-list))
+
+	   (new-alu (get-sparse-a-l-u *horizon*
+				      new-jac-states
+				      new-jac-controls
+				      *states*
+				      next-state
+				      *controls*
+				      *control-limits*
+				      *state-limits*))
+	   )
+      (destructuring-bind (a-mat l-vec u-vec ) new-alu
+	(mpc-matrix-update-alu a-mat l-vec u-vec))
+      (try-converge-non-linear *horizon*)
+      (mpc-matrix-update-q planned-traj)
+      (list states controls))))
+
+
+
+(defun mpc-next-goto-from (current-state objective)
+  (if (not (= (length *states*)
+	      (length current-state)
+	      (length objective)))
+      (progn
+	(print (length *states*))
+	(print (length current-state))
+	(print (length objective))
+	(error "Dimention of current state or objective wrong")))
+
+  ;; if (equal objective *target-state*)
+  (destructuring-bind (states controls) (solved-to-proper-list
+					 (solve-mpc)
+					 *states*
+					 *controls*
+					 *horizon*)
+    (let* ((next-state current-state)
+	   (next-control (car (cdr controls)))
+	   (planned-traj (trajectory-to-state-q next-state
+						objective))
+	   (state-control-list (mapcar (lambda (x y) (append x y))
+				       (cdr states) controls))
+	   
+	   (new-jac-states (mapcar (lambda (x) (apply *a-fn* x))
+				   state-control-list))
+	   
+	   (new-jac-controls (mapcar (lambda (x) (apply *b-fn* x))
+				     state-control-list))
+
+	   (new-alu (get-sparse-a-l-u *horizon*
+				      new-jac-states
+				      new-jac-controls
+				      *states*
+				      next-state
+				      *controls*
+				      *control-limits*
+				      *state-limits*)))
+      
+      (destructuring-bind (a-mat l-vec u-vec ) new-alu
+	(mpc-matrix-update-alu a-mat l-vec u-vec))
+      (mpc-matrix-update-q planned-traj)
+      (try-converge-non-linear 6)
+      (list states controls))))
+
+
+;; (defun mpc-change-objective-to (current-state objective)
+;;   (if (not (= (length *states*)
+;; 	       (length current-state)
+;; 	       (length objective)))
+;;       (error "Dimention of current state or objective wrong"))
+;;   (destructuring-bind (states controls) (solved-to-proper-list
+;; 					 (solve-mpc)
+;; 					 *states*
+;; 					 *controls*
+;; 					 *horizon*)
+;;     (setf *target-state* objective)
+    
+;;     (let* ((next-state current-state)
+;; 	   (planned-traj (trajectory-to-state-q next-state
+;; 						objective))
+;; 	   (state-control-list (mapcar (lambda (x y) (append x y))
+;; 				       (cdr states) controls))
+	   
+;; 	   (new-jac-states (mapcar (lambda (x) (apply *a-fn* x))
+;; 				   state-control-list))
+	   
+;; 	   (new-jac-controls (mapcar (lambda (x) (apply *b-fn* x))
+;; 				     state-control-list))
+
+;; 	   (new-alu (get-sparse-a-l-u *horizon*
+;; 				      new-jac-states
+;; 				      new-jac-controls
+;; 				      *states*
+;; 				      next-state
+;; 				      *controls*
+;; 				      *control-limits*
+;; 				      *state-limits*)))
+      
+;;       (destructuring-bind (a-mat l-vec u-vec ) new-alu
+;; 	(mpc-matrix-update-alu a-mat l-vec u-vec))
+;;       (try-converge-non-linear *horizon*)
+;;       (mpc-matrix-update-q planned-traj)
+;;       (list states controls))))
+
+
+
+(defvar *my-obj* nil)
+(setf *my-obj* *target-state*)
+
+(socket-clean)
+(socket-open-udp "localhost" 9999)
+
+(send-socket-data (floats->bytes '(-0.00)))
+
+(kill-recieve-thread)
+(make-recieve-theread)
+(read-udp-message)
+
+(loop while (not *initial-state*)
+      do
+	 (setf *initial-state* (read-udp-message)))
+(setf *initial-state* (read-udp-message))
+
+(setf *initial-control* '(0))
+
+(initialize-mpc-system)
 (try-converge-non-linear *horizon*)
 
-;; (run-mpc-once)
+(defvar *run-hz* 50)
 
-(loop for times from 0 to 600
-      do
-	 (destructuring-bind (states controls) (solved-to-proper-list
-						(solve-mpc)
-						*states*
-						*controls*
-						*horizon*)
-	   
-	   (let* ((next-state ;; (car (cdr states))
-			      (apply *f-fn* (append (car states) (car controls))))
-		  (next-control (car (cdr controls)))
-		  (planned-traj (trajectory-to-state-q next-state
-						       '(0 0 5.5 0)))
-		  (state-control-list (mapcar (lambda (x y) (append x y))
-					      (cdr states) controls))
-		  
-		  (new-jac-states (mapcar (lambda (x) (apply *a-fn* x))
-					  state-control-list))
-		  
-		  (new-jac-controls (mapcar (lambda (x) (apply *b-fn* x))
-					    state-control-list))
+(mpc-next-goto-from (read-udp-message)
+	       *my-obj*)
 
-		  (new-alu (get-sparse-a-l-u *horizon*
-					     new-jac-states
-					     new-jac-controls
-					     *states*
-					     next-state
-					     *controls*
-					     *control-limits*
-					     *state-limits*))
-		  )
-	     (destructuring-bind (a-mat l-vec u-vec ) new-alu
-	       (mpc-matrix-update-alu a-mat l-vec u-vec))
-	     (try-converge-non-linear 1)
-	     (mpc-matrix-update-q planned-traj))))
+;; (read-udp-message)
+
+(defvar *run-stuff* 'nil)
 
 
-(solved-to-proper-list (solve-mpc)
-		       *states*
-		       *controls*
-		       *horizon*)
+(defun mpc-loop ()
+  (print "runnnn")
+  (loop while *run-stuff*
+	do
+	   (destructuring-bind (predicted-trajectory predicted-control)
+	       (let ((this-state (read-udp-message)))
+		 (print "state: ")
+		 (print this-state)
+		 (mpc-next-goto-from this-state *my-obj*)
+		 )
+	     (print "control: ")
+	     (print (car predicted-control))
+	     (print (car predicted-trajectory))
+	     (if (> (car (car  predicted-control)) 100000)
+		 (error "we got bad value"))
+	     (send-socket-data (floats->bytes
+				(car predicted-control)))
+	     )
+	   (sleep 0.02)
 
+	))
+
+(setf *run-stuff* 1)
+
+(mpc-loop)
+
+(all-c-vars-free)
+
+(initialize-mpc-system)
+
+(defvar *mpc-loop-thread* nil)
+
+
+(socket-clean)
+(kill-recieve-thread)
+(setf *message-recieve-thread* nil)
+
+;; *state-weights*
+;; (setf *my-obj* '(0 0 2 0))
+
+
+;; (mpc-matrix-update-q 
+
+;;  (trajectory-to-state-q '(0 0 25 0)
+;; 			'(0 0 5 
+
+
+;; (setf *state-limits*
+;;       (state-limit-symbolic->list
+;;        *states*
+;;        -1.1
+;;        '((+ theta))
+;;        +1.1))
+
+
+;; (setf *control-limits*
+;;       '((-200.0) (200.0)))
